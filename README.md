f5f4dc963446778c5a86d112c5c6483a417fa03c
xiaoxx-mac <me@xiaoxx.cc>
2023-03-04 01:06:15 +0800

    第一个完整可用版本

---
+++ README.md
@@ -0,0 +1,75 @@
+## 关于这个项目
+
+在终端与 ChatGPT 聊天，包含美化的 Markdown 格式输出、上键历史检索、tokens 统计功能。
+
+![example](README.assets/small.gif)
+
+使用最新的 [gpt-3.5-turbo](https://platform.openai.com/docs/guides/chat/chat-completions-beta) 模型，也就是 ChatGPT 所使用的模型（并非前一代的 `text-davinci-003` 模型）。
+
+## 安装
+
+1. 克隆 Repo 并进入目录
+
+   ```shell
+   git clone https://github.com/xiaoxx970/chatgpt-in-terminal.git
+   cd ./chatgpt-in-terminal
+   ```
+
+2. 在项目根目录下的 `.env` 文件中写入 OPENAI_API_KEY 变量，内容如下
+
+   ```
+   OPENAI_API_KEY=你的API_KEY
+   ```
+
+   OpenAI 的密钥可在主页右上角点 `View API keys` 打开的页面中生成
+
+   ![image-20230303233352970](README.assets/image-20230303233352970.png)
+
+3. 通过 requirements.txt 安装依赖
+
+   ```shell
+   pip3 install -r requirements.txt
+   ```
+
+## 如何使用
+
+使用以下命令进行运行：
+
+```shell
+python3 chat.py
+```
+
+> 如果问题需要多行输入，以 `-m` 参数运行：
+>
+> ```shell
+> python3 chat.py -m
+> ```
+>
+> 多行模式下按回车切换下一行，如果在空行回车则提交问题
+>
+
+在聊天中，使用退出词可以结束本次会话，退出词有：
+
+```py
+['再见', 'bye', '结束', 'end', '退出', 'exit']
+```
+
+退出词将作为一个问题发送给 ChatGPT，在 GPT 回答后退出。
+
+也可使用 `Ctrl-C` 或者 `Ctrl-D` 立即退出
+
+退出后将显示本次聊天所使用的 tokens 统计，目前价格为 $0.002 / 1K tokens
+
+## 项目结构
+
+```
+├── README.md           # 项目说明文档
+├── chat.py             # 项目主要代码
+├── requirements.txt    # 依赖包列表
+├── chat.log						# 聊天后生成的对话日志
+└── .env								# 密钥存储文件
+```
+
+## 许可证
+
+该项目遵守[MIT许可证](LICENSE)。
--- chat.py
+++ chat.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import sys
 import openai
 import logging
 import readline
+from rich.console import Console
+from rich.markdown import Markdown
+from dotenv import load_dotenv
+load_dotenv()
 
-api_key = 'sk-xxx'
-logging.basicConfig(filename='chat.log', level=logging.DEBUG, encoding="UTF-8")
+api_key = os.environ.get("OPENAI_API_KEY")
+console = Console()
+logging.basicConfig(filename='chat.log', level=logging.INFO, encoding="UTF-8")
 
 
-class GPT:
+class CHATGPT:
     def __init__(self, api_key):
         openai.api_key = api_key
         self.messages = [
             {"role": "system", "content": "You are a helpful assistant."}]
-        print("Hi, welecome to chat with gpt.")
+        self.total_tokens = 0
 
     def send(self, message):
         self.messages.append({"role": "user", "content": message})
@@ -21,18 +28,42 @@ class GPT:
             model="gpt-3.5-turbo",
             messages=self.messages
         )
-        logging.info(response)
+        logging.info(f"Send: {self.messages}")
+        logging.info(f"Get: {response}")
+        self.total_tokens += response["usage"]["total_tokens"]
         reply = response["choices"][0]["message"]
         self.messages.append(reply)
         return reply
 
+    def get_total_tokens(self):
+        return self.total_tokens
 
-chatGPT = GPT(api_key)
-while True:
-    message = input("> ")
-    if message == "":
-        continue
-    reply = chatGPT.send(message)
-    print("ChatGPT: ", reply["content"])
-    if message in ['再见', 'bye', '结束', 'end', '退出', 'exit']:
-        exit(0)
+
+def multi_input(prompt):
+    lines = []
+    while True:
+        line = input(prompt)
+        if not line:
+            break
+        lines.append(line)
+    return '\n'.join(lines)
+
+
+chatGPT = CHATGPT(api_key)
+try:
+    console.print("Hi, welecome to chat with gpt.", style="dim")
+    while True:
+        message = "-m" in sys.argv and multi_input("> ") or input("> ")
+        if not message:
+            continue
+        with console.status("[bold cyan]ChatGPT is thinking...") as status:
+            reply = chatGPT.send(message)
+        console.print("ChatGPT: ", end='', style="bold cyan")
+        console.print(Markdown(reply["content"]), new_line_start=True)
+        if message.lower() in ['再见', 'bye', '结束', 'end', '退出', 'exit']:
+            break
+except (EOFError, KeyboardInterrupt):
+    print("\nExiting...")
+finally:
+    console.print(
+        f"[bright_magenta]Total tokens used: [bold]{chatGPT.get_total_tokens()}")
